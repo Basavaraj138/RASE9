e24f16893b1774c02265a54a7492eccfc4e25401
welcome <welcome@DESKTOP-VBRUKDA>
2022-11-14 23:20:32 +0530

    Ok..

---
+++ RASE9 GitHub/src/Restassured/Practise1.java
@@ -0,0 +1,20 @@
+package Restassured;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Practise1 {
+	@Test
+	public void test()
+	{
+		Reporter.log("Bginning of framework",true);
+	}
+    @Test
+    public void test1()
+    {
+    	Reporter.log("End of framework",false);
+    }
+}
+
+
+
